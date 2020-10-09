d50f02f040e1edbdd60463674afc09de63af895b
Vlas-2 <vposstovlass@gmail.com>
2020-10-09 23:56:36 +0300

    Initial commit.

---
+++ 2 10.10.2020.py
@@ -0,0 +1,45 @@
+a=int(input("Введите цифру"))
+if a==-10:
+    print(a, "+", 5, "=", a + 5)
+if a==-9:
+    print(a, "+", 5, "=", a + 5)
+if a==-8:
+    print(a, "+", 5, "=", a + 5)
+if a==-7:
+    print(a, "+", 5, "=", a + 5)
+if a==-6:
+    print(a, "+", 5, "=", a + 5)
+if a==-5:
+    print(a, "+", 5, "=", a + 5)
+if a==-4:
+    print(a, "+", 5, "=", a + 5)
+if a==-3:
+    print(a, "+", 5, "=", a + 5)
+if a==-2:
+    print(a, "+", 5, "=", a + 5)
+if a==-1:
+    print(a, "+", 5, "=", a + 5)
+if a==0:
+    print(a, "+", 5, "=", a + 5)
+if a==1:
+    print(a, "+", 5, "=", a + 5)
+if a==2:
+    print(a, "+", 5, "=", a + 5)
+if a==3:
+    print(a, "+", 5, "=", a + 5)
+if a==4:
+    print(a, "+", 5, "=", a + 5)
+if a==5:
+    print(a, "+", 5, "=", a + 5)
+if a==6:
+    print(a, "+", 5, "=", a + 5)
+if a==7:
+    print(a, "+", 5, "=", a + 5)
+if a==8:
+    print(a, "+", 5, "=", a + 5)
+if a==9:
+    print(a, "+", 5, "=", a + 5)
+if a>9:
+    print(a, "-", 10, "=", a - 10)
+if a<-10:
+    print(a, "-", 10, "=", a - 10)
---
+++ print2.py
@@ -0,0 +1,5 @@
+a=8
+b=4
+c=a+b
+print(a, "+", b, "=", a + b)
+print(a, "*", b, "=", a * b)
