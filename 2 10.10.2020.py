0e3077966ea54b24acbd0c0d60b6a77691e37cab
Vlas-1 <72249823+Vlas-1@users.noreply.github.com>
2020-10-10 11:17:35 +0300

    Update 2 10.10.2020.py

--- 2 10.10.2020.py
+++ 2 10.10.2020.py
@@ -1,45 +1,7 @@
-a=int(input("Введите цифру"))
-if a==-10:
-    print(a, "+", 5, "=", a + 5)
-if a==-9:
-    print(a, "+", 5, "=", a + 5)
-if a==-8:
-    print(a, "+", 5, "=", a + 5)
-if a==-7:
-    print(a, "+", 5, "=", a + 5)
-if a==-6:
-    print(a, "+", 5, "=", a + 5)
-if a==-5:
-    print(a, "+", 5, "=", a + 5)
-if a==-4:
-    print(a, "+", 5, "=", a + 5)
-if a==-3:
-    print(a, "+", 5, "=", a + 5)
-if a==-2:
-    print(a, "+", 5, "=", a + 5)
-if a==-1:
-    print(a, "+", 5, "=", a + 5)
-if a==0:
-    print(a, "+", 5, "=", a + 5)
-if a==1:
-    print(a, "+", 5, "=", a + 5)
-if a==2:
-    print(a, "+", 5, "=", a + 5)
-if a==3:
-    print(a, "+", 5, "=", a + 5)
-if a==4:
-    print(a, "+", 5, "=", a + 5)
-if a==5:
-    print(a, "+", 5, "=", a + 5)
-if a==6:
-    print(a, "+", 5, "=", a + 5)
-if a==7:
-    print(a, "+", 5, "=", a + 5)
-if a==8:
-    print(a, "+", 5, "=", a + 5)
-if a==9:
-    print(a, "+", 5, "=", a + 5)
-if a>9:
-    print(a, "-", 10, "=", a - 10)
-if a<-10:
-    print(a, "-", 10, "=", a - 10)
+for i in range(1,999):
+    a=int(input("Введите цифру"))
+    if a > -10 and a < 10:
+        a += 5
+    else:
+        a -= 10
+    print (a)
