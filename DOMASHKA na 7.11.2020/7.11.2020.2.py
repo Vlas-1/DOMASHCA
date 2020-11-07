cc5158dc359cdf9f58fb77ff108ddb4e0554eb68
Vlas-1 <72249823+Vlas-1@users.noreply.github.com>
2020-11-07 10:04:16 +0300

    Add files via upload

---
+++ DOMASHKA na 7.11.2020/7.11.2020.2.py
@@ -0,0 +1,3 @@
+stroka = input("Введите числа: ")
+stroka2 = sorted(stroka)
+print(stroka2)
---
+++ DOMASHKA na 7.11.2020/7.11.2020.py
@@ -0,0 +1,5 @@
+stroka = input("введите что то")
+simvolu=0
+for i in stroka:
+    simvolu+=1
+print("символов :",simvolu)
